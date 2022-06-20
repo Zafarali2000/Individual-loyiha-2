0edaef756a636c0564778279d138a63036003e1b
Zafarali2000 <99166219+Zafarali2000@users.noreply.github.com>
2022-06-20 18:19:47 +0500

    Add files via upload

---
+++ about.php
@@ -0,0 +1,193 @@
+<?php
+
+include 'components/connect.php';
+
+session_start();
+
+if(isset($_SESSION['user_id'])){
+   $user_id = $_SESSION['user_id'];
+}else{
+   $user_id = '';
+};
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Ma'lumotlar</title>
+
+   <link rel="stylesheet" href="https://unpkg.com/swiper@8/swiper-bundle.min.css" />
+   
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+   
+<?php include 'components/user_header.php'; ?>
+
+<section class="about">
+
+   <div class="row">
+
+      <div class="image">
+         <img src="images/about-img.svg" alt="">
+      </div>
+
+      <div class="content">
+         <h3>Biz sizga quydagilarni taklif qilamiz: </h3>
+         <p>Bizning mahsulotlar yuqori darajadagi sifatga ega.Bundan tashqari biz mashhur kompaniyalar bilan hamkorlikda ish olib boramiz, bu esa sifatni ta'minlaydi.Bizning sayt orqali o'zingizga kerakli mahsulotlarni topasiz!</p>
+         <a href="contact.php" class="btn">Biz bilan bog'lanish</a>
+      </div>
+
+   </div>
+
+</section>
+
+<section class="reviews">
+   
+   <h1 class="heading">Mijozning sharhlari</h1>
+
+   <div class="swiper reviews-slider">
+
+   <div class="swiper-wrapper">
+
+      <div class="swiper-slide slide">
+         <img src="images/pic-1.png" alt="">
+         <p>
+            Veb-sayt tshkil etuvchilarga oldindan rahmat aytamiz.Chunki biz uchun yaratilgan vebsaytda o'zimiz uchun kerakli bo'lgan barcha mahsulotlar topish mumkin
+         </p>
+                  <div class="stars">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star-half-alt"></i>
+         </div>
+         <h3>Eldor</h3>
+      </div>
+
+      <div class="swiper-slide slide">
+         <img src="images/pic-2.png" alt="">
+         <p>
+            Hammaga salom! Sayt yaratuvchilariga taklifim bor, ya'ni mahsulotlar haqida batafsilroq ma'lumotlar berilsa biz uchun yaxshi bo'lar edi.
+         </p>
+                  <div class="stars">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star-half-alt"></i>
+         </div>
+         <h3>Dildora</h3>
+      </div>
+
+      <div class="swiper-slide slide">
+         <img src="images/pic-3.png" alt="">
+         <p>
+            Taklif sifatida yozmoqda! Saytdagi barcha mahsulotlar sifatini va sonini oshirish kerak deb o'ylayman.Chunki mahsulotlar sifatli bo'lgani yaxshi.
+         </p>
+         <div class="stars">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star-half-alt"></i>
+         </div>
+         <h3>Sardor</h3>
+      </div>
+
+      <!-- <div class="swiper-slide slide">
+         <img src="images/pic-4.png" alt="">
+         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia tempore distinctio hic, iusto adipisci a rerum nemo perspiciatis fugiat sapiente.</p>
+         <div class="stars">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star-half-alt"></i>
+         </div>
+         <h3>Sophie Turner</h3>
+      </div> -->
+<!-- 
+      <div class="swiper-slide slide">
+         <img src="images/pic-5.png" alt="">
+         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia tempore distinctio hic, iusto adipisci a rerum nemo perspiciatis fugiat sapiente.</p>
+         <div class="stars">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star-half-alt"></i>
+         </div>
+         <h3>john deo</h3>
+      </div>
+
+      <div class="swiper-slide slide">
+         <img src="images/pic-6.png" alt="">
+         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia tempore distinctio hic, iusto adipisci a rerum nemo perspiciatis fugiat sapiente.</p>
+         <div class="stars">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star-half-alt"></i>
+         </div>
+         <h3>john deo</h3>
+      </div> -->
+
+   </div>
+
+   <div class="swiper-pagination"></div>
+
+   </div>
+
+</section>
+
+
+
+
+
+
+
+
+
+<?php include 'components/footer.php'; ?>
+
+<script src="https://unpkg.com/swiper@8/swiper-bundle.min.js"></script>
+
+<script src="js/script.js"></script>
+
+<script>
+
+var swiper = new Swiper(".reviews-slider", {
+   loop:true,
+   spaceBetween: 20,
+   pagination: {
+      el: ".swiper-pagination",
+      clickable:true,
+   },
+   breakpoints: {
+      0: {
+        slidesPerView:1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      991: {
+        slidesPerView: 3,
+      },
+   },
+});
+
+</script>
+
+</body>
+</html>
---
+++ footer.php
@@ -0,0 +1,41 @@
+<footer class="footer">
+
+   <section class="grid">
+
+      <div class="box">
+         <h3>Tezkor havolalar</h3>
+         <a href="home.php"> <i class="fas fa-angle-right"></i> Home</a>
+         <a href="about.php"> <i class="fas fa-angle-right"></i> Ma'lumotlar</a>
+         <a href="shop.php"> <i class="fas fa-angle-right"></i> Shop</a>
+         <a href="contact.php"> <i class="fas fa-angle-right"></i> Kontakt</a>
+      </div>
+
+      <div class="box">
+         <h3>Qo'shimcha havolalar</h3>
+         <a href="user_login.php"> <i class="fas fa-angle-right"></i> Login</a>
+         <a href="user_register.php"> <i class="fas fa-angle-right"></i> Ro'yxatdan o'tish</a>
+         <a href="cart.php"> <i class="fas fa-angle-right"></i> Xaridlar bo'limi</a>
+         <a href="orders.php"> <i class="fas fa-angle-right"></i> Buyurtmalar</a>
+      </div>
+
+      <div class="box">
+         <h3>Bog'lanish uchun</h3>
+         <a href="#"><i class="fas fa-phone"></i> +998 77 707 00 77</a>
+         <a href="#"><i class="fas fa-phone"></i> +998 77 808 00 88</a>
+         <a href="#"><i class="fas fa-envelope"></i> OnlineShop@gmail.com</a>
+         <a href="https://www.google.com/maps/@39.6798518,66.9818019,406m/data=!3m1!1e3"><i class="fas fa-map-marker-alt"></i> Samarqand, O'zbekiston </a>
+      </div>
+
+      <div class="box">
+         <h3>Ijtimoiy tarmoqlar</h3>
+         <a href="#"><i class="fab fa-facebook-f"></i>Facebook</a>
+         <a href="#"><i class="fab fa-twitter"></i>Twitter</a>
+         <a href="#"><i class="fab fa-instagram"></i>Instagram</a>
+         <a href="#"><i class="fab fa-telegram"></i>Telegram</a>
+      </div>
+
+   </section>
+
+   <!-- <div class="credit">&copy; copyright @ <?= date('Y'); ?> by <span>mr. web designer</span> | all rights reserved!</div> -->
+
+</footer>
